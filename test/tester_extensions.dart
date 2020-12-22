cf1077ce208437ff12e77753820128749e9dbf6d
SergioFerreira <sergio.ferreira@ctw.bmwgroup.com>
2020-12-22 12:15:14 +0000

    fixed pumpWidgetInApp

--- test/tester_extensions.dart
+++ test/tester_extensions.dart
@@ -2,7 +2,6 @@ import 'package:cresce_flutter_app/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ui_bits/ui_bits.dart';
 
 import 'fake_http_layer.dart';
 
@@ -11,7 +10,6 @@ extension TesterExtensions on WidgetTester {
       this.pumpAndSettle(const Duration(minutes: 1));
 
   Future pumpApp() async {
-    //useFakeHttpLayer();
     await this.pumpWidget(makeApp(
       overrideDependencies: (locator) {
         useFakeHttpLayer(locator);
@@ -21,30 +19,15 @@ extension TesterExtensions on WidgetTester {
   }
 
   Future pumpWidgetInApp(Widget widget) async {
-    //useFakeHttpLayer();
     await this.pumpWidget(
       makeApp(
         overrideDependencies: (locator) {
           useFakeHttpLayer(locator);
         },
-        home: Scaffold(
-          body: widget,
-        ),
+        home: Scaffold(body: widget),
       ),
     );
 
-    var themeFactory = ThemeFactory();
-
-    await this.pumpWidget(
-      MaterialApp(
-        theme: themeFactory.makeBlueTheme(),
-        home: themeFactory.makeHome(
-          child: Scaffold(
-            body: widget,
-          ),
-        ),
-      ),
-    );
     await this.waitForAnimationsToSettle();
   }
 }
