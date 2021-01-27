926752c969598a5f01490694d6524a5a5df8136d
SergioFerreira <sergio.ferreira@ctw.bmwgroup.com>
2021-01-27 14:34:33 +0000

    changed flow

--- test/employees/employees_carousel_test.dart
+++ test/employees/employees_carousel_test.dart
@@ -1,27 +1,30 @@
 import 'package:cresce_flutter_app/features/features.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ui_bits/ui_bits.dart';
+
 import '../tester_extensions.dart';
 
 void main() {
   group(EmployeeCarouselWidget, () {
-    testWidgets('pump page builds page', (tester) async {
+    testWidgets('on build fetch employees', (tester) async {
       await _pumpWidget(tester);
-
-      expect(find.byType(EmployeeCarouselWidget), findsOneWidget);
-    });
-    testWidgets('on page build fetch employees', (tester) async {
-      await _pumpWidget(tester);
-      await tester.pumpAndSettle();
 
       expect(find.byType(BitCarousel), findsOneWidget);
       expect(find.byType(BitThumbnail), findsOneWidget);
     });
+    testWidgets('tapping an employee displays PinPad', (tester) async {
+      await _pumpWidget(tester);
+
+      await tester.tap(find.byType(BitThumbnail));
+
+      expect(find.byType(BitPinPad), findsOneWidget);
+    });
   });
 }
 
-Future _pumpWidget(WidgetTester tester) {
-  return tester.pumpWidgetInApp(
+Future _pumpWidget(WidgetTester tester) async {
+  await tester.pumpWidgetInApp(
     EmployeeCarouselWidget(),
   );
+  await tester.pumpAndSettle();
 }
